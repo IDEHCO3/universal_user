f5d7a1df5da4b4d0ae6516451969a64e7d6ae77a
idehco3 <rogerio.borba17@gmail.com>
2015-10-14 12:01:26 -0300

    adding uuid

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+
+    user_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unicode=True)
