a63622ce4a9d25795e9f0e061846a9f875504c25
aluizio <aluizio777@gmail.com>
2017-01-04 19:54:47 -0200

    Added login by facebook with angularjs

--- authentication/static/authentication/user_controller.js
+++ authentication/static/authentication/user_controller.js
@@ -5,6 +5,74 @@
             $interpolateProvider.endSymbol('$}');
         });
 
+    app.factory('fbAuth', function($rootScope, $http){
+        return {
+            watchLoginChange: function() {
+
+                var _self = this;
+
+                this.getUserInfo = function(){
+                    FB.api('/me', function(res) {
+                        $rootScope.$apply(function() {
+                            $rootScope.fbuser = res;
+                        });
+                    });
+                };
+
+                this.getToken = function(authResponse){
+                    $http.post(url_fb_token, {accessToken: authResponse.accessToken })
+                        .success(function(data){
+                            console.log(data);
+                        })
+                        .error(function(data){
+                            console.log("Error: token wasn't taken!");
+                        });
+                };
+
+                FB.Event.subscribe('auth.authResponseChange', function(res) {
+                    if (res.status === 'connected') {
+                        console.log("logged!!!!!!!!!!!!!");
+                        _self.getUserInfo();
+                        _self.getToken(res.authResponse);
+                    }
+                    else {
+                        console.log("not logged!!!!!!!!!!!!!");
+                    }
+
+                });
+
+            }
+        }
+    });
+
+    app.run(['$rootScope', '$window', 'fbAuth', function($rootScope, $window, fbAuth) {
+
+        $rootScope.fbuser = {};
+
+        $window.fbAsyncInit = function() {
+
+            FB.init({
+                appId      : '1071687446243206',
+                xfbml      : true,
+                version    : 'v2.8'
+            });
+
+            fbAuth.watchLoginChange();
+        };
+
+        (function(d){
+            var js,
+            id = 'facebook-jssdk',
+            ref = d.getElementsByTagName('script')[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement('script');
+            js.id = id;
+            js.async = true;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            ref.parentNode.insertBefore(js, ref);
+        }(document));
+    }]);
+
     app.factory('authInterceptor', function ($rootScope, $q, $window) {
         return {
             request: function (config) {
@@ -27,7 +95,7 @@
         $httpProvider.interceptors.push('authInterceptor');
     });
 
-    app.controller('UserCtrl', function ($scope, $http, $window) {
+    app.controller('UserCtrl', ['$scope', '$http', '$window', 'fbAuth', function ($scope, $http, $window, fbAuth) {
         $scope.user = {username: '', password: '', name: ''};
         $scope.message = '';
         $scope.next = url_next;
@@ -42,6 +110,10 @@
                     console.log(data);
                 });
         }
+
+        $scope.testOut = function(){
+            console.log("what?");
+        };
 
         $scope.submit = function () {
             $http.post(url_authetication_token, $scope.user)
@@ -71,5 +143,5 @@
         $scope.isAutheticated = function(){
             return $window.sessionStorage.token != null;
         };
-    });
+    }]);
 })();
